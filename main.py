565cc47e28646fbdb230a21774f00c815601c5cc
Mark M <135105725+MarriottMark@users.noreply.github.com>
2025-05-19 04:13:11 +0000

    created Teacher class

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ class Person:
         self.LastName = LastName
     
     def PrintFullName(self):
-        return
+        print(self.FirstName + " " + self.LastName)
 
 class Student(Person):
     def __init__(self, FirstName, LastName, StudentID, HouseGroup):
@@ -15,21 +15,65 @@ class Student(Person):
     
     def EnrollClass(self, SubjectName):
         self.Subjects.append(SubjectName)
-        
+
     def ShowClasses(self):
         for sub in self.Subjects:
             print(sub)
 
 
 class Parent(Person):
-    def __init__(self, Occupation, Alumni):
+    def __init__(self, Occupation):
         self.Occupation = Occupation
-        self.Alumni = Alumni
+        self.Alumni = False
     
+    def ShowOccupation(self):
+        print(self.Occupation)
+
+
+    def SetAlumni(self, Alumni):
+        self.Alumni = Alumni
+       
+    def DisplayAlumni(self):
+        if self.Alumni == True:
+            print("This parent is an alumni")
+        else:
+            print("This parent is not an alumni")
+
+
 class Subject():
     def __init__(self):
         return
+    
+
+class Teacher():
+    def __init__(self, Title):
+        self.Title = Title
+        self.Subject = []
+        
+    def ShowClasses(self):
+        for sub in self.Subject:
+            print(sub)
+    
+    def EnrollClass(self, SubjectName):
+        self.Subject.append(SubjectName)
+
+# Main program
  
-s1 = Student("Slirting", "Knox", "at least 3", "Broadton")
-s1.EnrollClass("gup")
+s1 = Student("Troy", "Harcoan", "6", "Heber")
+s1.EnrollClass("Maths Advanced")
+s1.EnrollClass("English Advanced")
+s1.EnrollClass("Ancient History")
+s1.EnrollClass("Music")  
+s1.EnrollClass("Software Engineering")
+s1.EnrollClass("Biology")
 s1.ShowClasses()
+s1.PrintFullName()
+print("p1")
+p1 = Parent("Makes lights")
+p1.SetAlumni(True)
+p1.ShowOccupation()
+p1.DisplayAlumni()
+print("T1")
+t1 = Teacher("Mr.")
+t1.EnrollClass("Maths Advanced")
+t1.ShowClasses()
